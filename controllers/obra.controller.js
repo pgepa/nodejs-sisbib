43a98355aaef7b0192a11461c5f53140a8e75c3c
abudluan <abudluan@gmail.com>
2022-05-23 11:38:09 -0300

    chore: mecanismo de busca

--- controllers/obra.controller.js
+++ controllers/obra.controller.js
@@ -77,9 +77,9 @@ const findOne = (req, res) => {
 
 // Retrieve some certificates from the database according to restriction
 const findSome = (req, res) => {
-    const id = req.body.id;
-    const condition = id ? { id: { [Op.like]: `%${id}%` } } : null;
-    Certificate.findAll({ where: condition })
+    const termo = req.params.termo;
+    const condition = termo ? { descritores: { [Op.like]: `%${termo}%` } } : null;
+    Obra.findAll({ where: condition })
     .then((data) => {
         res.status(200).send(data);
     })
--- routes/obra.routes.js
+++ routes/obra.routes.js
@@ -6,11 +6,12 @@ const controller = require('../controllers/obra.controller');
 
 obrasRouter.get('/all', controller.findAll);
 obrasRouter.get('/:id', controller.findOne);
+obrasRouter.get('/search/:termo', controller.findSome);
 
 obrasRouter.post('/add', controller.create);
 obrasRouter.post('/:id', controller.update);
 
-obrasRouter.post('/some', controller.findSome);
+
 
 obrasRouter.delete('/:id', controller.exclude);
 
